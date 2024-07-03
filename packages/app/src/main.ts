2b492af9f413de369000b1fe817a79f9318b938b
Mirza <zanuarmirza@gmail.com>
2024-07-03 22:56:54 +0700

    hello from wasm

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import * as wasm from "hello";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -22,3 +23,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+wasm.greet();
